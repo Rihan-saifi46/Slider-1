deeb3efda9a55fb6033bcca2d4cceeaf40a9e150
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-25 22:04:14 +0530

    ad

--- src.js
+++ src.js
@@ -1,3 +1,9 @@
 //step 1: get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
+
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
