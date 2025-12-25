d363b2c46d4a4db407ad0055315f7518b81f98fc
Rihan-saifi46 <kharihan786k@gmail.com>
2025-12-25 22:04:29 +0530

    d

--- src.js
+++ src.js
@@ -7,3 +7,11 @@ let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function(){
+    showSlider('next');    
+}
